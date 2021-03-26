56bacffcf6551df67d6a33cf46b7dea34c8fcc16
Tom Lafford <tom@laffyco.net>
2021-03-26 14:10:05 +0000

    Initial commit

---
+++ tom-lafford/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div class="columns pt-5">
+      <div class="column is-center is-4 is-offset-4">
+        <Card
+          :jobTitle="job"
+          :name="name"
+          :description="description"
+          :email="email"
+          :gitHub="gitHubUrl"
+          :linkedIn="linkedInUrl"
+          :imageAlt="imageAlt"
+          :imgSrc="imgSrc"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card.vue";
+import "bulma/css/bulma.css";
+
+export default {
+  name: "App",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      linkedInUrl: "https://www.linkedin.com/in/thomas-lafford/",
+      gitHubUrl: "https://github.com/Laffini",
+      email: "tom&commat;laffyco&period;net",
+      name: "Tom Lafford",
+      job: "Aerospace Software Development Engineer",
+      description: `Multi-disciplined apprentice Software Engineer. Working at BAE Systems
+          and studying a BSc (Hons) in Software Engineering from the University of
+          Central Lancashire.`,
+      imageAlt: "A picture of Tom Lafford",
+      imgSrc: "../assets/tomlafford.png",
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  background-color: #85dcba;
+}
+</style>
---
+++ tom-lafford/src/components/Card.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card is-small">
+    <div class="card-image">
+      <figure class="image is-horizontal-center is-4by3">
+        <img src="../assets/tomlafford.png" v-bind:alt="imageAlt" />
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-4">{{ name }}</p>
+          <p class="subtitle is-6">{{ jobTitle }}</p>
+        </div>
+      </div>
+
+      <div class="content">
+        {{ description }}
+        <p class="pt-4">📧: <a v-html="email"></a></p>
+        <p>
+          👨‍💻: <a target="_blank" rel="noopener" v-bind:href="gitHub">GitHub</a>
+        </p>
+        <p>
+          🌐:
+          <a target="_blank" rel="noopener" v-bind:href="linkedIn">LinkedIn</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    description: String,
+    linkedIn: String,
+    gitHub: String,
+    email: String,
+    name: String,
+    jobTitle: String,
+    imageAlt: String,
+  },
+};
+</script>
+
+
